e8082820dbb26976daf76a4cc21da9c1d54f97e4
Yaro WebDev <yg@think.studio>
2023-09-22 16:29:41 +0300

    Remob facede docs

--- src/Facades/GDCInfoManager.php
+++ src/Facades/GDCInfoManager.php
@@ -4,13 +4,6 @@ namespace GDCInfo\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static \Google\AdsApi\AdManager\v202208\OrderService createOrderService()
- * @method static \Google\AdsApi\AdManager\v202208\LineItemService createLineItemService()
- * @method static \Google\AdsApi\AdManager\v202208\CreativeService createCreativeService()
- * @method static \Google\AdsApi\AdManager\v202208\CreativeSetService createCreativeSetService()
- * @method static \Google\AdsApi\AdManager\v202208\LineItemCreativeAssociationService createLineItemCreativeAssociationService()
- */
 class GDCInfoManager extends Facade
 {
     protected static function getFacadeAccessor()
